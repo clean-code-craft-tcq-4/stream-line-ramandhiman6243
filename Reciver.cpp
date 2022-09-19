386d3e754656ce030a0f3f6c13a6b8400f7afa3d
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-09-19 15:52:35 +0200

    Update Reciver.cpp

--- Reciver.cpp
+++ Reciver.cpp
@@ -1,22 +1,56 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <cctype>
-#include <streambuf>
-#include <ostream>
-#include <string>
-#include <fstream>
 #include "json/json.h"
 
+
+
 using namespace std;
+using namespace Json;
 
-int main() {
 
-	std::ifstream file_input("senderOutputExample.json");
-	Json::Reader reader;
-	Json::Value root;
-	reader.parse(file_input, root);
-	cout << root;
 
-    return 0;
+int main()
+{
+    char inputString[] = R"(
+        {  
+            "readings": [
+            {
+              "temperature": 86.17,
+              "chargeRate": 71
+            },
+            {
+              "temperature": 5.85,
+              "chargeRate": 74
+            },
+            {
+              "temperature": 30.12,
+              "chargeRate": 61
+            },
+            {
+              "temperature": 83.48,
+              "chargeRate": 40
+            },
+            {
+              "temperature": 45.87,
+              "chargeRate": 28
+            }
+          ]
+        }
+    )";
+
+
+
+   Value root;
+    Reader reader;
+    reader.parse(inputString, root);
+
+
+
+   Value readings = root["readings"];
+
+
+
+   for (int i = 0; i < readings.size(); i++) {
+        cout << "Temperature: " << readings[i]["temperature"] << endl;
+        cout << "Charge Rate: " << readings[i]["chargeRate"] << endl << endl;
+    }
 }

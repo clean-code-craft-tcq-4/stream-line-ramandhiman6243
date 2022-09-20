3bf6762e4f6647686484eb2f76e71706732b6b0a
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-09-20 12:20:46 +0200

    Update Sender_Parser

--- Sender_Parser.cpp
+++ Sender_Parser.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include "json/json.h"
 #include <fstream>
+#include "Sender_Parser.hpp"
 
 using namespace std;
 using namespace Json;
 
+
 int main()
 {
+	Reading Total_Readings[G_TOTAL_READINGS];
 	std::ifstream file_input("senderOutputExample.json");
 	Json::Reader reader;
 	Json::Value root;
@@ -17,6 +20,8 @@ int main()
 	for (int i = 0; i < readings.size(); i++) {
 		cout << "Temperature: " << readings[i]["temperature"] << endl;
 		cout << "Charge Rate: " << readings[i]["chargeRate"] << endl << endl;
+		Total_Readings[i].Temperature = (float)(readings[i]["temperature"]);
+		Total_Readings[i].Charge_Rate = (int)(readings[i]["chargeRate"]);
 	}
 
 	return 0;
---
+++ Sender_Parser.hpp
@@ -0,0 +1,11 @@
+#ifndef SENDER_PARSER_HPP
+#define SENDER_PARSER_HPP
+
+struct Reading
+{
+	float Temperature;
+	int Charge_Rate;
+};
+
+const int G_TOTAL_READINGS = 5;
+#endif

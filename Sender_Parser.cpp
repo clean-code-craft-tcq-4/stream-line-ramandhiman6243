a96c06c8e51159b75e1b277ce2a47e5b72407d15
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-09-26 16:37:23 +0200

    Update Sender

--- Sender_Parser.cpp
+++ Sender_Parser.cpp
@@ -2,14 +2,14 @@
 #include "json/json.h"
 #include <fstream>
 #include "Sender_Parser.hpp"
+#include <bits/stdc++.h>
 
 using namespace std;
 using namespace Json;
 
 
-int main()
-{
-	Reading Total_Readings[G_TOTAL_READINGS];
+void jsonparser(struct Reading &Total_Readings) {
+
 	std::ifstream file_input("senderOutputExample.json");
 	Json::Reader reader;
 	Json::Value root;
@@ -18,13 +18,40 @@ int main()
 	Value readings = root["readings"];
 
 	for (int i = 0; i < readings.size(); i++) {
-		//cout << "Temperature: " << readings[i]["temperature"] << endl;
-		//cout << "Charge Rate: " << readings[i]["chargeRate"] << endl << endl;
-		Total_Readings[i].Temperature = readings[i]["temperature"].asFloat();
-		Total_Readings[i].Charge_Rate = readings[i]["chargeRate"].asInt();
-		cout << "Temperature: " << Total_Readings[i].Temperature << endl;
-		cout << "Charge Rate: " << Total_Readings[i].Charge_Rate << endl << endl;
+		Total_Readings.Temperature[i] = readings[i]["temperature"].asFloat();
+		Total_Readings.Charge_Rate[i] = readings[i]["chargeRate"].asInt();
+		cout << "Temperature: " << Total_Readings.Temperature[i] << endl;
+		cout << "Charge_Rate: " << Total_Readings.Charge_Rate[i] << endl;
 	}
 
-	return 0;
 }
+
+void Get_Maximum(struct Reading temp_chargerate, int number_of_readings, Maximum &MaxOfAttribute) {
+
+	MaxOfAttribute.MAX_TEMP = MAX_Reading(temp_chargerate.Temperature, number_of_readings);
+	MaxOfAttribute.MAX_CHARGE_RATE = MAX_Reading(temp_chargerate.Charge_Rate, number_of_readings);
+
+}
+
+void Get_Minimum(struct Reading temp_chargerate, int number_of_readings, Minimum &MinOfAttribute) {
+
+	MinOfAttribute.MIN_TEMP = MIN_Reading(temp_chargerate.Temperature, number_of_readings);
+	MinOfAttribute.MIN_CHARGE_RATE = MIN_Reading(temp_chargerate.Charge_Rate, number_of_readings);
+
+}
+
+int main(){
+
+	Reading Total_Readings;
+	Maximum MaxOfAttribute;
+	Minimum MinOfAttribute;
+	jsonparser(Total_Readings);
+	Get_Maximum(Total_Readings, G_TOTAL_READINGS, MaxOfAttribute);
+	Get_Minimum(Total_Readings, G_TOTAL_READINGS, MinOfAttribute);
+
+	cout << "max.charge" << MaxOfAttribute.MAX_CHARGE_RATE;
+	cout << "max.TEMP" << MaxOfAttribute.MAX_TEMP;
+
+}
+
+
--- Sender_Parser.hpp
+++ Sender_Parser.hpp
@@ -1,11 +1,45 @@
 #ifndef SENDER_PARSER_HPP
 #define SENDER_PARSER_HPP
 
-struct Reading
-{
-	float Temperature;
-	int Charge_Rate;
-};
+#include <bits/stdc++.h>
+#include <iostream>
 
 const int G_TOTAL_READINGS = 5;
+
+struct Reading
+{
+	float Temperature[G_TOTAL_READINGS] = {0};
+	int Charge_Rate[G_TOTAL_READINGS] = {0};
+};
+
+struct Maximum
+{
+	float MAX_TEMP;
+	int MAX_CHARGE_RATE;
+};
+
+struct Minimum
+{
+	float MIN_TEMP;
+	int MIN_CHARGE_RATE;
+};
+
+void jsonparser(struct Reading &Total_Readings);
+
+void Get_Maximum(struct Reading temp_chargerate, int number_of_readings, Maximum &MaxOfAttribute);
+
+void Get_Minimum(struct Reading temp_chargerate, int number_of_readings, Minimum &MinOfAttribute);
+
+template <typename temp_chargerate>
+typename MAX_Reading(temp_chargerate arr[], int readings)
+{
+  return *max_element(arr, arr+readings);
+}
+
+template <typename temp_chargerate>
+typename MIN_Reading(temp_chargerate arr[], int readings)
+{
+  return *min_element(arr, arr+readings);
+}
+
 #endif

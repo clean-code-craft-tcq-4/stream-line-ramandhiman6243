f6a5135cd0553cb6f447f8960f063da62c688fa7
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-09-30 14:12:59 +0200

    Update Receiver_Test_Cases_main.cpp

--- Receiver/Receiver_Test_Cases_main.cpp
+++ Receiver/Receiver_Test_Cases_main.cpp
@@ -5,16 +5,19 @@
 #include "Receiver.hpp"
 using namespace std;
 
+int Sampled_Charge_Rate[] = { 4, 5, 8, 9, 12, 15, 91, 19, 99, 0 };
+float Sampled_Temperature[] = { 4.4, 3.5, 8.8, 1.9, 1.2, 1.5, 9.1, 1.4, 9.9, 0.0 };
+
 // As a first step, only considering to detect continuous ranges in the samples
 TEST_CASE("POSITIVE TEST CASE 1 - Maximum Charge Rate/Temperature ") {
-
-    //REQUIRE(SampleRangeReadings(Current_Samples, Current_Samples_size, 2) == true);
 
 	Maximum MaxOfAttribute;
     Reading Sampled_Reading;
 
-    Sampled_Reading.Charge_Rate[] = { 4, 5, 8, 9, 12, 15, 91, 19, 99, 0 };
-    Sampled_Reading.Temperature[] = { 4.4, 3.5, 8.8, 1.9, 1.2, 1.5, 9.1, 1.4, 9.9, 0.0 };
+    for (int i = 0; i < 10; i++) {
+        Sampled_Reading.Charge_Rate[i] = Sampled_Charge_Rate[i];
+        Sampled_Reading.Temperature[i] = Sampled_Temperature[i];
+    }
 
 	Get_Maximum(Sampled_Reading, MaxOfAttribute);
 

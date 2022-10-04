c11021bb3f22cb032f52c53972f51acefb6bcddd
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-10-04 10:31:39 +0200

    Update Receiver_Test_Cases_main.cpp

--- Receiver/Receiver_Test_Cases_main.cpp
+++ Receiver/Receiver_Test_Cases_main.cpp
@@ -6,9 +6,9 @@
 using namespace std;
 
 int Sampled_Charge_Rate[] = { 4, 5, 8, 9, 12, 15, 91, 19, 99, 0 };
-float Sampled_Temperature[] = { 4.4, 3.5, 8.8, 1.9, 1.2, 1.5, 9.1, 1.4, 9.9, 0.0 };
+float Sampled_Temperature[] = { 4.4, 3.5, 8.8, 1.9, 1.2, 1.5, 9.1, 1.4, 9.9, 0.1 };
 
-// As a first step, only considering to detect continuous ranges in the samples
+// Maximum Charge Rate - Temperature
 TEST_CASE("POSITIVE TEST CASE 1 - Maximum Charge Rate/Temperature ") {
 
 	Maximum MaxOfAttribute;
@@ -27,4 +27,25 @@ TEST_CASE("POSITIVE TEST CASE 1 - Maximum Charge Rate/Temperature ") {
 
     REQUIRE(MaxOfAttribute.MAX_CHARGE_RATE == 99);
     REQUIRE(MaxOfAttribute.MAX_TEMP == 9.9f);
+}
+
+// Minimum Charge Rate - Temperature
+TEST_CASE("POSITIVE TEST CASE 2 - Minimum Charge Rate/Temperature ") {
+
+	Minimum MinOfAttribute;
+    Reading Sampled_Reading;
+
+    for (int i = 0; i < 10; i++) {
+        Sampled_Reading.Charge_Rate[i] = Sampled_Charge_Rate[i];
+        Sampled_Reading.Temperature[i] = Sampled_Temperature[i];
+    }
+
+	Get_Minimum(Sampled_Reading, MinOfAttribute);
+
+	cout << "Minimum charge rate = " << MinOfAttribute.MIN_CHARGE_RATE << endl;
+	cout << "Minimum Temperature = " << MinOfAttribute.MIN_TEMP << endl;
+	cout << endl;
+
+    REQUIRE(MinOfAttribute.MIN_CHARGE_RATE == 0);
+    REQUIRE(MinOfAttribute.MIN_TEMP == 0.1f);
 }

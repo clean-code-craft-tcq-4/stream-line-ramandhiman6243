2dc52987614e8dbd68cad3a132a67d416d0a8523
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-10-10 16:44:46 +0200

    Update Receiver_Test_Cases_main.cpp

--- Receiver/Receiver_Test_Cases_main.cpp
+++ Receiver/Receiver_Test_Cases_main.cpp
@@ -9,7 +9,7 @@ int Sampled_Charge_Rate[] = { 4, 5, 8, 9, 12, 15, 91, 19, 99, 0 };
 float Sampled_Temperature[] = { 4.4, 3.5, 8.8, 1.9, 1.2, 1.5, 9.1, 1.4, 9.9, 0.1 };
 
 // Maximum Charge Rate - Temperature
-TEST_CASE("POSITIVE TEST CASE 1 - Maximum Charge Rate/Temperature ") {
+TEST_CASE("Maximum Charge Rate/Temperature ") {
 
 	Maximum MaxOfAttribute;
     Reading Sampled_Reading;
@@ -30,7 +30,7 @@ TEST_CASE("POSITIVE TEST CASE 1 - Maximum Charge Rate/Temperature ") {
 }
 
 // Minimum Charge Rate - Temperature
-TEST_CASE("POSITIVE TEST CASE 2 - Minimum Charge Rate/Temperature ") {
+TEST_CASE("Minimum Charge Rate/Temperature ") {
 
 	Minimum MinOfAttribute;
     Reading Sampled_Reading;
@@ -48,4 +48,30 @@ TEST_CASE("POSITIVE TEST CASE 2 - Minimum Charge Rate/Temperature ") {
 
     REQUIRE(MinOfAttribute.MIN_CHARGE_RATE == 0);
     REQUIRE(MinOfAttribute.MIN_TEMP == 0.1f);
+}
+
+// Simple Moving Average
+TEST_CASE("Simple Moving Average of latest 5 values Charge Rate/Temperature ") {
+    
+    SimpleMovingAverage SMA_Temperature(G_LastnReadings);
+ 	SimpleMovingAverage SMA_ChargeRate(G_LastnReadings);
+    Reading Sampled_Reading;
+
+    for (int i = 0; i < 10; i++) {
+        Sampled_Reading.Charge_Rate[i] = Sampled_Charge_Rate[i];
+        Sampled_Reading.Temperature[i] = Sampled_Temperature[i];
+    }
+
+ 	for (double SMA_of_Temperature : Sampled_Reading.Temperature) {
+		SMA_Temperature.addData(SMA_of_Temperature);
+ 	cout << "Simple Moving Average of temperature reading = " << SMA_Temperature.getMean() << endl;
+    REQUIRE(SMA_Temperature.getMean() == 3.96f);
+
+ 	cout << endl;
+
+ 	for (double SMA_of_ChargeRate : Sampled_Reading.Charge_Rate) {
+ 		SMA_ChargeRate.addData(SMA_of_ChargeRate);
+	cout << "Simple Moving Average of charge rate reading = " << SMA_ChargeRate.getMean() << endl; }
+        REQUIRE(SMA_ChargeRate.getMean() == 7.6f);
+
 }
